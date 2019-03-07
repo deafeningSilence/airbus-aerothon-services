e68b5689191675feebb439eac2060ebf0b2e57b4
Abhijeeth Padarthi <rkinabhi@gmail.com>
2019-03-07 11:59:51 +0530

    fixed query object

--- src/main/java/com/airbus/aerothon/controllers/AerothonController.java
+++ src/main/java/com/airbus/aerothon/controllers/AerothonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -47,7 +48,7 @@ public class AerothonController {
 
 	// FLIGHT - INDEX
 	@GetMapping("/flight")
-	public List<Flight> getFlights(@RequestBody FlightQueryRequestDTO flightQueryRequestDTO) {
+	public List<Flight> getFlights(@ModelAttribute FlightQueryRequestDTO flightQueryRequestDTO) {
 		return flightService.getFlights(flightQueryRequestDTO);
 	}
 
