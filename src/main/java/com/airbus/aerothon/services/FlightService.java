6cd89f30318165cabb43a499c69988a653d6c0e5
Abhijeeth Padarthi <rkinabhi@gmail.com>
2019-03-07 13:56:28 +0530

    fixed crud

--- src/main/java/com/airbus/aerothon/services/FlightService.java
+++ src/main/java/com/airbus/aerothon/services/FlightService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.airbus.aerothon.dto.in.FlightQueryRequestDTO;
+import com.airbus.aerothon.entities.impl.Airport;
 import com.airbus.aerothon.entities.impl.Flight;
 import com.airbus.aerothon.entities.impl.Program;
 import com.airbus.aerothon.repository.FlightRepository;
+import com.airbus.aerothon.repository.impl.AirportRepository;
 import com.airbus.aerothon.repository.impl.ProgramRepository;
 import com.google.common.collect.Lists;
 
@@ -21,6 +23,9 @@ public class FlightService {
 	@Autowired
 	ProgramRepository programRepository;
 	
+	@Autowired
+	AirportRepository airportRepository;
+	
 	public List<Flight> getFlights(FlightQueryRequestDTO flightQueryRequestDTO) {
 		return flightRepository.getFlights(flightQueryRequestDTO);
 		//return Lists.newArrayList(flightRepository.findAll());
@@ -28,14 +33,22 @@ public class FlightService {
 
 	public Flight addFlight(Flight flight) {
 		Program program = programRepository.findById(flight.getProgram().getId()).get();
+		Airport source = airportRepository.findById(flight.getSourceAirport().getId()).get();
+		Airport destination = airportRepository.findById(flight.getDestinationAirport().getId()).get();
 		flight.setProgram(program);
+		flight.setSourceAirport(source);
+		flight.setDestinationAirport(destination);
 		flightRepository.save(flight);
 		return flight;
 	}
 
 	public Flight updateFlight(Integer flightId, Flight flight) {
 		Program program = programRepository.findById(flight.getProgram().getId()).get();
+		Airport source = airportRepository.findById(flight.getSourceAirport().getId()).get();
+		Airport destination = airportRepository.findById(flight.getDestinationAirport().getId()).get();
 		flight.setProgram(program);
+		flight.setSourceAirport(source);
+		flight.setDestinationAirport(destination);
 		flightRepository.save(flight);
 		return flight;
 	}
